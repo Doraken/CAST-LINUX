bb6b815067aeafd68d3648f5967b59a7c589d1d3
arnaud <arnaud@crampet.net>
2020-05-08 15:02:14 +0200

    update basse patch

--- Cast-update.sh
+++ Cast-update.sh
@@ -2,4 +2,4 @@ git reset --hard
 git pull  --rebase --no-autostash
 chmod +x /srv/admin/CAST-LINUX/*.sh
 chmod +x /srv/admin/CAST-LINUX/bin/*
-/srv/admin/CAST-LINUX/init-all.sh
+/srv/admin/CAST-LINUX/bin/init-all.sh
